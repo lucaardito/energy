341992daa17301d292e331cb33b17cd37d1b73cb
Marco Sciuto <marco.sciuto@outlook.com>
2015-11-11 18:14:59 +0100

    Implementation of new tagging algorithm (to be continued)

--- android/density.R
+++ android/density.R
@@ -27,23 +27,24 @@ file.create(file = out.filename)
 for(i in 1:length(data.sources)){
   data <- read.delim2(paste0(base.folder,data.sources[i]), header = FALSE, skip = 7)
   data$V1 <- data$V1*5.03
-  dens = density(data$V1,adjust=.2)
+  dens = density(data$V1,adjust=2)
   dens$peaks = peaks(dens$y,9) & dens$y > mean(dens$y)
   
   dens.gap = subset(data.frame(dens[c("x","y")]),y==min(dens$y))
   gap.min = min(dens.gap$x)
   gap.max = max(dens.gap$x)
   idle.power = dens$x[dens$y==max(dens$y)]
-  discr.power = mean(dens.gap$x)
+  #discr.power = mean(dens.gap$x)
   dens$peaks = which(peaks(dens$y) & dens$y>mean(dens$y))
-  between.peaks = seq(dens$peaks[1],dens$peaks[2])
+  between.peaks = seq(dens$peaks[2],dens$peaks[3])
   gap = intersect(which(dens$y==min(dens$y[between.peaks])),between.peaks)
 
   gap.min = min(dens$x[gap])
   gap.max = max(dens$x[gap])
   
   idle.power = dens$x[dens$peaks[1]]
-  discr.power = mean(dens$x[gap])
+  #discr.power = mean(dens$x[gap])
+  discr.power = dens$x[gap] + (dens$x[dens$peaks[3]]-dens$x[dens$peaks[2]])/4
 
   write(paste0(syscall[i],": ",dens$x[dens$peaks]),
         file = out.filename,
@@ -52,7 +53,7 @@ for(i in 1:length(data.sources)){
   dens.gap = data.frame(dens[c("x","y")])[gap,]
   
   p <- ggplot(data,aes(x=V1))
-  p <- p + geom_line(stat="density",adjust=.2) + expand_limits(y=0)
+  p <- p + geom_line(stat="density",adjust=2) + expand_limits(y=0)
   p <- p + geom_line(data=dens.gap,aes(x=x,y=y),color="red")
   p <- p + geom_point(data=data.frame(x=dens$x,y=dens$y)[dens$peaks,],aes(x=x,y=y),col="red",size=5, pch=1)
   #p <- p + annotate("rect", xmin=min(dens$x), xmax=gap.min, ymin=0, ymax=max(dens$y), alpha=.1,
@@ -68,11 +69,11 @@ for(i in 1:length(data.sources)){
   #ggsave(paste0(base.folder,"plot/density_",syscall[i],".png"), plot = p)
   
   # Save power distribution to file
-  #png(filename = paste0(base.folder,"plot/data_",syscall[i],".png"), width = 8600, height = 1000)
   rp <- ggplot(data,aes(x=c(1:length(data$V1)),y=V1))
-  rp <- rp + geom_line()
+  rp <- rp + geom_point()
   for(j in 1:length(dens$peaks)){
-    rp <- rp + annotate("segment", x=0,xend=length(data$V1),y=dens$x[dens$peaks[j]],yend=dens$x[dens$peaks[j]], color="red",linewidth=2)
+    rp <- rp + annotate("segment", x=0,xend=length(data$V1),y=dens$x[dens$peaks[j]],yend=dens$x[dens$peaks[j]], color="green",linewidth=2)
   }
+  rp <- rp + annotate("segment", x=0,xend=length(data$V1),y=discr.power,yend=discr.power, color="red",linewidth=2)
   #ggsave(paste0(base.folder,"plot/power_",syscall[i],".png"), plot = rp)
 }
