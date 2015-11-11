8d6ce7491bb291c236c53feec623858f00163f07
Marco Sciuto <supermac89@gmail.com>
2015-11-11 23:54:54 +0100

    Implementation of tagging algorithm

--- android/density.R
+++ android/density.R
@@ -2,6 +2,8 @@ setwd("~")
 library("ggplot2")
 
 base.folder = "./git/energy/android/data/"
+noise = 35
+
 data.sources <- list.files(base.folder,pattern = "syscall_.*\\.txt",full.names = F)
 syscall = sub("syscall_", "",sub("\\.txt$","",data.sources))
 
@@ -21,38 +23,53 @@ peaks <- function(series, span = 3, do.pad = TRUE) {
 }
 ##########################################################################
 
-out.filename = paste0(base.folder,"power_density_peaks_values.txt")
-file.create(file = out.filename)
+#out.filename = paste0(base.folder,"power_density_peaks_values.txt")
+#file.create(file = out.filename)
 
-for(i in 1:length(data.sources)){
+#for(i in 1:length(data.sources)){
+  i=2
   data <- read.delim2(paste0(base.folder,data.sources[i]), header = FALSE, skip = 7)
-  data$V1 <- data$V1*5.03
-  dens = density(data$V1,adjust=2)
+  data$P <- data$V1*5.03
+  dens = density(data$P,adjust=2)
   dens$peaks = peaks(dens$y,9) & dens$y > mean(dens$y)
-  
-  dens.gap = subset(data.frame(dens[c("x","y")]),y==min(dens$y))
-  gap.min = min(dens.gap$x)
-  gap.max = max(dens.gap$x)
-  idle.power = dens$x[dens$y==max(dens$y)]
-  #discr.power = mean(dens.gap$x)
+
   dens$peaks = which(peaks(dens$y) & dens$y>mean(dens$y))
   between.peaks = seq(dens$peaks[2],dens$peaks[3])
   gap = intersect(which(dens$y==min(dens$y[between.peaks])),between.peaks)
-
-  gap.min = min(dens$x[gap])
-  gap.max = max(dens$x[gap])
   
   idle.power = dens$x[dens$peaks[1]]
   #discr.power = mean(dens$x[gap])
-  discr.power = dens$x[gap] + (dens$x[dens$peaks[3]]-dens$x[dens$peaks[2]])/4
-
-  write(paste0(syscall[i],": ",dens$x[dens$peaks]),
-        file = out.filename,
-        append = TRUE)
-  
+  discr.power = dens$x[gap] + (dens$x[dens$peaks[3]]-dens$x[dens$peaks[2]])/7
   dens.gap = data.frame(dens[c("x","y")])[gap,]
   
-  p <- ggplot(data,aes(x=V1))
+  ## tag as IDLE or WORK based on discr.power threshold
+  tag.levels = c("IDLE","WORK","PAUSE","NOISE")
+  data$tag = factor(tag.levels[findInterval(data$P,c(0,discr.power))],tag.levels)
+  
+  ## assign unique id to runs
+  data$runid = cumsum(c(0,abs(diff(as.numeric(data$tag))) ) )
+  
+  ## mark runs with few (<=noise) samples as NOISE
+  id.noise = unique(data$runid)[table(data$runid)<=noise]
+  data$tag[data$runid%in%id.noise] = "NOISE"
+  
+  ## discard NOISE runs --- better to set the lower peak value instead of removing the sample?
+  data=subset(data,tag!="NOISE")
+  
+  # re-assign run ids
+  data$runid = cumsum(c(0,abs(diff(as.numeric(data$tag))) ) )
+  
+  # find energy markers
+  id.markers = unique(data$runid)[table(data$runid)>=4700 & table(data$runid)<=5000] # ~ length of the marker
+  data$tag[(data$runid %in% id.markers) & (data$tag=="WORK")] = "PAUSE"
+  
+  # Take sets between two consecutive PAUSE and extract data
+  
+  #write(paste0(syscall[i],": ",dens$x[dens$peaks]),
+  #      file = out.filename,
+  #      append = TRUE)
+  
+  p <- ggplot(data,aes(x=P))
   p <- p + geom_line(stat="density",adjust=2) + expand_limits(y=0)
   p <- p + geom_line(data=dens.gap,aes(x=x,y=y),color="red")
   p <- p + geom_point(data=data.frame(x=dens$x,y=dens$y)[dens$peaks,],aes(x=x,y=y),col="red",size=5, pch=1)
@@ -69,11 +86,13 @@ for(i in 1:length(data.sources)){
   #ggsave(paste0(base.folder,"plot/density_",syscall[i],".png"), plot = p)
   
   # Save power distribution to file
-  rp <- ggplot(data,aes(x=c(1:length(data$V1)),y=V1))
-  rp <- rp + geom_point()
+  rp <- ggplot(data,aes(x=c(1:length(data$P)),y=P))
+  rp <- rp + geom_line(size=.5)
+  #rp <- rp + geom_point(size=.5)
   for(j in 1:length(dens$peaks)){
-    rp <- rp + annotate("segment", x=0,xend=length(data$V1),y=dens$x[dens$peaks[j]],yend=dens$x[dens$peaks[j]], color="green",linewidth=2)
+    rp <- rp + annotate("segment", x=0,xend=length(data$P),y=dens$x[dens$peaks[j]],yend=dens$x[dens$peaks[j]], color="green",linewidth=2)
   }
-  rp <- rp + annotate("segment", x=0,xend=length(data$V1),y=discr.power,yend=discr.power, color="red",linewidth=2)
+  rp <- rp + annotate("segment", x=0,xend=length(data$P),y=discr.power,yend=discr.power, color="red",linewidth=2)
+  
   #ggsave(paste0(base.folder,"plot/power_",syscall[i],".png"), plot = rp)
-}
+#}
